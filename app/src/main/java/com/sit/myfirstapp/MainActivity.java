7c26c85b3e8a4b4ddeb4104a97ba3a8015a9d2fd
sergey <vir.ser.f13@gmail.com>
2018-12-28 17:07:32 +0200

    changes

--- app/src/main/java/com/sit/myfirstapp/MainActivity.java
+++ app/src/main/java/com/sit/myfirstapp/MainActivity.java
@@ -3,18 +3,14 @@ package com.sit.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.text);
         findViewById(R.id.btn).setOnClickListener((view) -> startActivity(new Intent(MainActivity.this, AnotherActivity.class)));
     }
 
