70617c49840c4761664790affacd465c27ccb1e6
sergey <vir.ser.f13@gmail.com>
2018-12-28 18:22:00 +0200

    color changing button

--- app/src/main/java/com/sit/myfirstapp/MainActivity.java
+++ app/src/main/java/com/sit/myfirstapp/MainActivity.java
@@ -9,32 +9,32 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnChangeColor;
-
+    Button btnChangeColor2;
+    Button btnChangeColor3;
     private final boolean red = true;
     private final boolean black = false;
-
-    private boolean isRed = true;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn).setOnClickListener((view) -> startActivity(new Intent(MainActivity.this, AnotherActivity.class)));
 
         btnChangeColor = findViewById(R.id.btn_change_color);
 
         btnChangeColor.setOnClickListener(v -> {
-            if (isRed(btnChangeColor)) {
-                btnChangeColor.setBackgroundResource(R.color.red);
-                btnChangeColor.setTag(red);
-            } else {
-                btnChangeColor.setBackgroundResource(R.color.black);
-                btnChangeColor.setTag(black);
-            }
+            changeBtnColor(btnChangeColor);
+            changeBtnColor(btnChangeColor2);
+            changeBtnColor(btnChangeColor3);
         });
     }
-
-
+    private void changeBtnColor(Button btnChangeColor) {
+        if (isRed(btnChangeColor)) {
+            btnChangeColor.setBackgroundResource(R.color.black);
+            btnChangeColor.setTag(black);
+        } else {
+            btnChangeColor.setBackgroundResource(R.color.red);
+            btnChangeColor.setTag(red);
+        }
+    }
     private boolean isRed(View v) {
         return (boolean) v.getTag();
     }
