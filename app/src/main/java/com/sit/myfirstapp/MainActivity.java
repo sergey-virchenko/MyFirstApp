033bbe12bcf64cad72b9e6b30e02bfa7c4cbc8ed
sergey <vir.ser.f13@gmail.com>
2018-12-28 18:13:43 +0200

    color changing button

--- app/src/main/java/com/sit/myfirstapp/MainActivity.java
+++ app/src/main/java/com/sit/myfirstapp/MainActivity.java
@@ -3,11 +3,16 @@ package com.sit.myfirstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnChangeColor;
+
+    private final boolean red = true;
+    private final boolean black = false;
+
     private boolean isRed = true;
 
     @Override
@@ -17,13 +22,21 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener((view) -> startActivity(new Intent(MainActivity.this, AnotherActivity.class)));
 
         btnChangeColor = findViewById(R.id.btn_change_color);
+
         btnChangeColor.setOnClickListener(v -> {
-            if (isRed) {
+            if (isRed(btnChangeColor)) {
                 btnChangeColor.setBackgroundResource(R.color.red);
+                btnChangeColor.setTag(red);
             } else {
                 btnChangeColor.setBackgroundResource(R.color.black);
+                btnChangeColor.setTag(black);
             }
         });
+    }
+
+
+    private boolean isRed(View v) {
+        return (boolean) v.getTag();
     }
 
 }
